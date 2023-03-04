dbc100d9777cb936418ce00363c7b9adc1b01735
Ayush Mittal <ayush.mittal@outlook.com>
2023-03-04 22:24:37 +0530

    signup, signin

--- application.go
+++ application.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Response struct {
@@ -24,7 +25,7 @@ type Todo struct {
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
 	Complete    bool      `json:"complete"`
-	UserId      User      `json:"user"`
+	UserId      int       `json:"userId"`
 }
 
 type User struct {
@@ -35,19 +36,34 @@ type User struct {
 	LastName  string    `json:"lastName"`
 }
 
-type UserPayload struct {
+type SignInPayload struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
+type SignUpPayload struct {
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+type UserResponse struct {
+	ID        uuid.UUID `json:"id"`
+	Email     string    `json:"email"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+}
+
 type Claims struct {
-	Username string `json:"username"`
+	User UserResponse `json:"user"`
 	jwt.RegisteredClaims
 }
 
 var jwtSignKey = []byte("sdsdf@q212")
 
 var todos []*Todo
+var users []*User
 
 func ListTodos(c *fiber.Ctx) error {
 	return c.JSON(Response{Status: 200, Data: todos})
@@ -69,10 +85,14 @@ func AddTodo(c *fiber.Ctx) error {
 }
 
 func GetTodoById(c *fiber.Ctx) error {
+	fmt.Println(c.Locals("user"))
 	id := c.Params("id", "")
 	fmt.Println(id)
 	if id == "" {
-		return c.Status(http.StatusBadRequest).JSON(Response{Status: 400, Error: errors.New("Please provide id")})
+		return c.Status(http.StatusBadRequest).JSON(Response{
+			Status: 400,
+			Error:  errors.New("Please provide id"),
+		})
 	}
 
 	for _, todo := range todos {
@@ -126,16 +146,36 @@ func DeleteTodo(c *fiber.Ctx) error {
 }
 
 func SignIn(c *fiber.Ctx) error {
-	var payload UserPayload
+	var payload SignInPayload
 	// Decode and bind body to payload
 	if err := c.BodyParser(&payload); err != nil {
 		return c.Status(http.StatusBadRequest).JSON(Response{Status: 400, Message: err.Error()})
 	}
 	// Check for valid username and password
+	var user *User
+	for _, userData := range users {
+		if userData.Email == payload.Username {
+			user = userData
+			break
+		}
+	}
+	if user == nil {
+		return c.Status(http.StatusBadRequest).JSON(Response{Status: 400, Message: "User Not Found"})
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(payload.Password)); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(Response{Status: 400, Message: "Password doesnot match"})
+	}
+
 	// Generate JWT token, if valid
 	expirationTime := time.Now().Add(5 * time.Minute)
 	claims := &Claims{
-		Username: payload.Username,
+		User: UserResponse{
+			ID:        user.ID,
+			Email:     user.Email,
+			FirstName: user.FirstName,
+			LastName:  user.LastName,
+		},
 		RegisteredClaims: jwt.RegisteredClaims{
 			ExpiresAt: jwt.NewNumericDate(expirationTime),
 		},
@@ -156,6 +196,29 @@ func SignIn(c *fiber.Ctx) error {
 	return c.JSON(Response{Status: 200, Data: tokenString})
 }
 
+func SignUp(c *fiber.Ctx) error {
+	var payload SignUpPayload
+	if err := c.BodyParser(&payload); err != nil {
+		c.Status(http.StatusBadRequest).JSON(Response{Status: http.StatusBadRequest, Error: err})
+	}
+
+	password, err := bcrypt.GenerateFromPassword([]byte(payload.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.Status(http.StatusBadRequest).JSON(Response{Status: http.StatusBadRequest, Error: err})
+	}
+
+	user := &User{
+		ID:        uuid.New(),
+		Email:     payload.Username,
+		Password:  string(password),
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+	}
+	users = append(users, user)
+
+	return c.JSON(Response{Status: http.StatusOK, Data: user})
+}
+
 func Logout(c *fiber.Ctx) error {
 	c.Cookie(&fiber.Cookie{
 		Name:    "token",
@@ -168,10 +231,9 @@ func Logout(c *fiber.Ctx) error {
 func ValidateToken(c *fiber.Ctx) error {
 	skipPaths := []string{
 		"/api/todo/signin",
+		"/api/todo/signup",
 	}
 	for _, path := range skipPaths {
-		fmt.Println(path)
-		fmt.Println(c.Path())
 		if c.Path() == path {
 			return c.Next()
 		}
@@ -181,6 +243,24 @@ func ValidateToken(c *fiber.Ctx) error {
 	if token == "" {
 		return c.Status(http.StatusUnauthorized).JSON(Response{Status: http.StatusUnauthorized, Message: "Not Authorized"})
 	}
+
+	claims := &Claims{}
+	parseToken, err := jwt.ParseWithClaims(token, claims, func(t *jwt.Token) (interface{}, error) {
+		return jwtSignKey, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			return c.Status(http.StatusUnauthorized).JSON(Response{})
+		}
+
+		return c.Status(http.StatusBadRequest).JSON(Response{})
+	}
+
+	if !parseToken.Valid {
+		return c.Status(http.StatusUnauthorized).JSON(Response{})
+	}
+
+	c.Locals("user", claims.User)
 
 	return c.Next()
 }
@@ -203,6 +283,7 @@ func main() {
 	todoApi.Delete("/:id", DeleteTodo)
 
 	todoApi.Post("/signin", SignIn)
+	todoApi.Post("/signup", SignUp)
 
 	app.Listen(":8080")
 }
